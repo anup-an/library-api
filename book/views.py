43997ea3d9e8c9b7c6027401818a81a93ce27fc1
anup-an <friek_an@yahoo.com>
2023-06-08 11:34:18 +0300

    Add ordering by title to book list endpoint

--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from book.models import Book
-from rest_framework import routers, serializers, viewsets
+from rest_framework import filters, viewsets
 from book.serializers import BookSerializer
 
 
@@ -9,3 +9,6 @@ from book.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title']
+    ordering = ['title']
