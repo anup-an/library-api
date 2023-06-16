49971ca09489ba198a704a165710ee02765ab398
anup-an <friek_an@yahoo.com>
2023-06-16 11:26:35 +0300

    Add functions to

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,61 +1,78 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { ApiData, failure, success } from "src/types/ApiTypes";
 import { StructError, assert } from "superstruct";
+import { logoutUser } from "src/api/user";
 
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFToken";
-axios.defaults.withCredentials = true;
-
+axios.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (error.response && error.response.status === 401) {
+      await logoutUser();
+    }
+    throw error;
+  }
+);
 
 export class ApiError {
-    public title: string;
-    public description: string | object[];
-    public statusCode?: number;
+  public title: string;
+  public description: string | object[];
+  public statusCode?: number;
 
-    constructor(title: string, description: string | object[], statusCode?: number) {
-        if (statusCode) {
-            this.statusCode = statusCode;
-        }
-        this.title = title;
-        this.description = description;
+  constructor(
+    title: string,
+    description: string | object[],
+    statusCode?: number
+  ) {
+    if (statusCode) {
+      this.statusCode = statusCode;
     }
+    this.title = title;
+    this.description = description;
+  }
 }
 
 export const parseError = <T>(error: any): ApiData<T, ApiError> => {
-    let title;
-    let description;
-    let status;
-    if (error instanceof StructError) {
-        title = 'DecoderError';
-        description = `${error.failures()[0].message} for ${error.failures()[0].key} field`;
-    } else {
-        title = error.response?.data.title || 'UnknownError';
-        description = error.response?.data.description || 'Something went wrong';
-        status = error.response?.status;
-    }
-    return failure(new ApiError(title, description, status));
+  let title;
+  let description;
+  let status;
+  if (error instanceof StructError) {
+    title = "DecoderError";
+    description = `${error.failures()[0].message} for ${
+      error.failures()[0].key
+    } field`;
+  } else {
+    title = error.response?.data.title || "UnknownError";
+    description = error.response?.data.description || "Something went wrong";
+    status = error.response?.status;
+  }
+  return failure(new ApiError(title, description, status));
 };
 
-
-
-const makeRequest = async <T, E>(fetchFunc: () => Promise<AxiosResponse<T>>, decoder: any) => {
-    try {
-        const { data } = await fetchFunc();
-        assert(data, decoder);
-        return success<T>(data);
-    } catch (error: unknown) {
-        return parseError<T>(error);
-    }
+const makeRequest = async <T, E>(
+  fetchFunc: () => Promise<AxiosResponse<T>>,
+  decoder: any
+) => {
+  try {
+    const { data } = await fetchFunc();
+    assert(data, decoder);
+    return success<T>(data);
+  } catch (error: unknown) {
+    return parseError<T>(error);
+  }
 };
 
 const axiosRequest = {
-    get: async <T, E>(url: string, decoder: any, config?: AxiosRequestConfig) => {
-        return makeRequest<T, E>(() => axios.get(url, config), decoder);
-    },
-    post: async <T, E>(url: string, decoder: any, payload?: any, config?: AxiosRequestConfig) => {
-        return makeRequest<T, E>(() => axios.post(url, payload, config), decoder);
-    },
+  get: async <T, E>(url: string, decoder: any, config?: AxiosRequestConfig) => {
+    return makeRequest<T, E>(() => axios.get(url, config), decoder);
+  },
+  post: async <T, E>(
+    url: string,
+    decoder: any,
+    payload?: any,
+    config?: AxiosRequestConfig
+  ) => {
+    return makeRequest<T, E>(() => axios.post(url, payload, config), decoder);
+  },
 };
 
 export default axiosRequest;
-
--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,15 +1,33 @@
 import { IgnoreResponseDecoder } from "src/decoders/common";
 import axios, { ApiError } from "./axios";
-import { LoginPayload, RegisterPayload } from "src/types/common";
+import { LoginPayload, RegisterPayload } from "src/types/authenticate";
 
 export const loginUser = async (payload: LoginPayload) => {
-  return await axios.post<unknown, ApiError>("http://127.0.0.1:8000/api/user/login", IgnoreResponseDecoder, payload);
+  return await axios.post<unknown, ApiError>(
+    "http://127.0.0.1:8000/api/user/login",
+    IgnoreResponseDecoder,
+    payload
+  );
 };
 
 export const registerUser = async (payload: RegisterPayload) => {
-  return await axios.post<unknown, ApiError>("http://127.0.0.1:8000/api/user/register", IgnoreResponseDecoder, payload);
+  return await axios.post<unknown, ApiError>(
+    "http://127.0.0.1:8000/api/user/register",
+    IgnoreResponseDecoder,
+    payload
+  );
 };
 
 export const logoutUser = async () => {
-  return await axios.post<unknown, ApiError>("http://127.0.0.1:8000/api/user/logout", IgnoreResponseDecoder);
+  return await axios.post<unknown, ApiError>(
+    "http://127.0.0.1:8000/api/user/logout",
+    IgnoreResponseDecoder
+  );
+};
+
+export const fetchUser = async () => {
+  return await axios.get<unknown, ApiError>(
+    "http://127.0.0.1:8000/api/user/",
+    IgnoreResponseDecoder
+  );
 };
