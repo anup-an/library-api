882f404fcfec85a3289d94f3a5b162ff8683f17f
anup-an <friek_an@yahoo.com>
2023-06-30 08:47:19 +0300

    Add filtering options to book list page

--- client/src/components/SearchAndFilter/index.tsx
+++ client/src/components/SearchAndFilter/index.tsx
@@ -1,12 +1,20 @@
 import { useContext, useState } from "react";
 import { DispatchContext } from "src/App";
 import { SEARCH_BOOKS } from "src/actions/book";
-import "./SearchAndFilter.scss"
+import { Option, SelectOption } from "src/components/ui/Select";
+import "./SearchAndFilter.scss";
+import Select from "src/components/ui/Select";
 
-const SearchAndFilter = () => {
+interface IProps {
+  selectOptions: SelectOption[];
+}
+
+const SearchAndFilter = (props: IProps) => {
   const [search, setSearch] = useState<string>("");
-  const [searchFields, setSearchFields] = useState<string[]>([]);
+  const [searchFields, setSearchFields] = useState<string[]>(["title"]);
+  const [filter, setFilter] = useState({});
   const { dispatch } = useContext(DispatchContext);
+  const { selectOptions } = props;
 
   const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event?.preventDefault();
@@ -15,13 +23,16 @@ const SearchAndFilter = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log(filter);
     dispatch({
       type: SEARCH_BOOKS,
-      payload: { search: search, search_fields: searchFields },
+      payload: { search: search, search_fields: searchFields, filter: filter },
     });
   };
 
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchCheckboxChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     event.target.checked
       ? setSearchFields([...searchFields, event.target.value])
       : setSearchFields(
@@ -29,35 +40,52 @@ const SearchAndFilter = () => {
         );
   };
 
+  const handleSelect = (option: { optionKey: string; value: any }) => {
+    setFilter({ ...filter, [option.optionKey]: JSON.parse(option.value) });
+  };
+
   return (
-      <form onSubmit={handleSubmit} className="search-filter">
-        <div className="search-filter__search">
-        <label htmlFor="search" >
+    <form onSubmit={handleSubmit} className="search-filter">
+      <div className="search-filter__search">
+        <label htmlFor="search">
           <input
             id="search"
             name="search"
             placeholder="Search"
             onChange={onSearchInputChange}
+            className="search-input"
           />
         </label>
-        <div>
-          <input
-            value="title"
-            type="checkbox"
-            onChange={handleCheckboxChange}
-          />
-          <span>Title</span>
+        <div className="search-fields">
+          <div>
+            <input
+              value="title"
+              type="checkbox"
+              defaultChecked={searchFields.includes("title")}
+              onChange={handleSearchCheckboxChange}
+            />
+            <span>Title</span>
+          </div>
+          <div>
+            <input
+              value="series"
+              type="checkbox"
+              defaultChecked={searchFields.includes("series")}
+              onChange={handleSearchCheckboxChange}
+            />
+            <span>Series</span>
+          </div>
         </div>
-        <div>
-          <input
-            value="series"
-            type="checkbox"
-            onChange={handleCheckboxChange}
-          />
-          <span>Series</span>
-              </div>
-              </div>
-      </form>
+      </div>
+      <div className="search-filter__filter">
+        {selectOptions.map((option) => (
+          <div key={option.name} className="option">
+            <span>{option.name}</span>
+            <Select selectConfig={option} handleSelect={handleSelect} />
+          </div>
+        ))}
+      </div>
+    </form>
   );
 };
 
--- client/src/views/BookListPage/index.tsx
+++ client/src/views/BookListPage/index.tsx
@@ -1,10 +1,40 @@
 import BookList from "src/components/BookList";
 import SearchAndFilter from "src/components/SearchAndFilter";
+import { SelectOption } from "src/components/ui/Select";
+import "./BookListPage.scss";
+
+const selectOptions: SelectOption[] = [
+  {
+    name: "Language",
+    value: "language",
+    options: [
+      { name: "All", value: "" },
+      { name: "English", value: { name: "English" } },
+      { name: "German", value: { name: "German" } },
+      { name: "French", value: { name: "French" } },
+      { name: "Persian", value: { name: "Persian" } },
+      { name: "Arabic", value: { name: "Arabic" } }
+    ],
+  },
+  {
+    name: "Book format",
+    value: "book_format",
+    options: [
+      { name: "All", value: "" },
+      { name: "Ebook", value: "e" },
+      { name: "Hardcover", value: "h" },
+      { name: "Kindle edition", value: "k" },
+      { name: "Mass Market Paperback", value: "m" },
+    ],
+  },
+];
 
 const BookListPage = () => {
   return (
-    <div>
-      <SearchAndFilter />
+    <div className="booklist-page">
+      <div>
+        <SearchAndFilter selectOptions={selectOptions} />
+      </div>
       <BookList />
     </div>
   );
