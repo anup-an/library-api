aa0f700b43706150bb43650475201ae1c5a4e2cd
anup-an <friek_an@yahoo.com>
2023-10-10 14:48:49 +0300

    Update exception handler function in backed

--- server/user/exceptions.py
+++ server/user/exceptions.py
@@ -1,7 +1,22 @@
-from rest_framework.exceptions import ValidationError, AuthenticationFailed
-from rest_framework.views import exception_handler
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from rest_framework.response import Response
+from rest_framework import exceptions
 
 
 def base_exception_handler(exc, context):
-    return Response(data={'title': exc.default_code, 'description': exc.detail}, status=exc.status_code)
+
+    if isinstance(exc, exceptions.APIException):
+        return Response(
+            data={
+                'title': exc.default_code,
+                'description': exc.detail},
+            status=exc.status_code)
+
+    return Response(
+        data={
+            'title': 'error',
+            'description': 'An error occurred'
+        },
+        status=500
+    )
