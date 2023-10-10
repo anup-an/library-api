0a5ca62e7ce4c618b6e60f041eddc18cb2cc9667
anup-an <friek_an@yahoo.com>
2023-10-10 23:27:06 +0300

    Remove borrow button

--- client/src/components/BookList/BookCard/index.tsx
+++ client/src/components/BookList/BookCard/index.tsx
@@ -1,20 +1,15 @@
 import {
+  Box,
+  Button,
   Card,
   CardBody,
+  CardFooter,
+  Divider,
+  Heading,
   Image,
   Stack,
-  Heading,
-  Divider,
-  CardFooter,
-  ButtonGroup,
-  Button,
-  Box,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ApiError } from "src/api/axios";
-import { borrowBook } from "src/api/user";
-import { applyApiEffect } from "src/types/ApiTypes";
 import { Book } from "src/types/book";
 
 interface IProps {
@@ -24,31 +19,16 @@ interface IProps {
 const BookCard = (props: IProps) => {
   const { book } = props;
   const navigate = useNavigate();
-  const [isBorrowing, setIsBorrowing] = useState<boolean>(false);
 
   const openDetails = () => {
     navigate(`/books/${book.id}`);
-  };
-
-  const handleBorrowBook = async () => {
-    setIsBorrowing(true);
-    const response = await borrowBook(book.id);
-    applyApiEffect(
-      response,
-      (data) => {
-        setIsBorrowing(false);
-      },
-      (error: ApiError) => {
-        setIsBorrowing(false);
-      }
-    );
   };
 
   return (
     <>
       <Card>
         <CardBody maxW="sm">
-          <Box display="flex" justifyContent="center">
+          <Box display="flex" justifyContent="center" onClick={openDetails}>
             <Image
               src={book.book_image || undefined}
               alt={book.title}
@@ -64,20 +44,9 @@ const BookCard = (props: IProps) => {
         </CardBody>
         <Divider />
         <CardFooter>
-          <ButtonGroup spacing="2">
-            <Button
-              variant="solid"
-              colorScheme="blue"
-              onClick={handleBorrowBook}
-              isLoading={isBorrowing}
-              loadingText="Borrow"
-            >
-              Borrow
-            </Button>
-            <Button variant="ghost" colorScheme="blue" onClick={openDetails}>
-              See details
-            </Button>
-          </ButtonGroup>
+          <Button variant="ghost" colorScheme="blue" onClick={openDetails}>
+            View details
+          </Button>
         </CardFooter>
       </Card>
     </>
