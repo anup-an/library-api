c94f5cdd1f0209c59ad26ad9f85f09fabb274e3e
anup-an <friek_an@yahoo.com>
2023-10-16 01:55:28 +0300

    Update register page

--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ApiError } from "src/api/axios";
 import { registerUser } from "src/api/user";
-import Button from "src/components/ui/Button";
-import { isSuccess } from "src/types/ApiTypes";
+import { applyApiEffect, isSuccess } from "src/types/ApiTypes";
+
+interface RegistrationDetails {
+  username: string;
+  password: string;
+}
+
+const MARGIN = 4;
 
 const Register = () => {
   const history = useNavigate();
@@ -10,43 +20,72 @@ const Register = () => {
     username: "",
     password: "",
   });
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
     event?.preventDefault();
+    setLoading(true);
     const response = await registerUser(registerDetails);
+    applyApiEffect(
+      response,
+      (data) => {
+        setLoading(false);
+        history("/login");
+      },
+      (error: ApiError) => {
+        setLoading(false);
+      }
+    );
+
     if (isSuccess(response)) {
       history("/login");
     }
   };
 
-  const updateDetails = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event?.preventDefault();
+  const handleInputChnage = (
+    name: keyof RegistrationDetails,
+    value: RegistrationDetails[typeof name]
+  ) => {
     setRegisterDetails({
       ...registerDetails,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
   };
   return (
     <div>
       <form onSubmit={handleRegister}>
-        <label htmlFor="username">
-          <input
-            id="username"
+        <FormControl isRequired marginTop={MARGIN}>
+          <FormLabel>E-mail</FormLabel>
+          <Input
             name="username"
-            placeholder="Type an e-mail"
-            onChange={updateDetails}
+            value={registerDetails.username}
+            onChange={(e) => handleInputChnage("username", e.target.value)}
+            type="email"
+            borderColor="black"
+            placeholder="example@example.com"
           />
-        </label>
-        <label htmlFor="password">
-          <input
-            id="password"
+        </FormControl>
+        <FormControl isRequired marginTop={MARGIN}>
+          <FormLabel>Password</FormLabel>
+          <Input
             name="password"
-            placeholder="Type a password"
+            value={registerDetails.password}
+            onChange={(e) => handleInputChnage("password", e.target.value)}
             type="password"
-            onChange={updateDetails}
+            placeholder="Type your password here"
+            borderColor="black"
           />
-        </label>
-        <Button buttonText="Regsiter" type="submit" />
+        </FormControl>
+        <Button
+          colorScheme="teal"
+          variant="solid"
+          type="submit"
+          marginTop={MARGIN}
+          isLoading={loading}
+          loadingText="Signing up"
+        >
+          Sign Up
+        </Button>
       </form>
     </div>
   );
