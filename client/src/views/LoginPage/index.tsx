660fab40684983f411f22ec6f4f2f060bfb56d49
anup-an <friek_an@yahoo.com>
2023-10-16 01:20:50 +0300

    Update login page ui

--- client/src/views/LoginPage/index.tsx
+++ client/src/views/LoginPage/index.tsx
@@ -4,8 +4,8 @@ import Login from "src/components/Login";
 
 const LoginPage = () => {
   return (
-    <Box padding={4}>
-      <Heading mb="10">Login</Heading>
+    <Box padding={4} width="50%">
+      <Heading mb="10" fontSize="30px" marginTop="60px">Login</Heading>
       <Login />
     </Box>
   );
