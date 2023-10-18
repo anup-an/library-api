2d47afcf3fbfe8e730644f00801aa56a8b45c240
anup-an <friek_an@yahoo.com>
2023-10-18 11:39:09 +0300

    Add footer to pages

--- client/src/views/RootLayout/index.tsx
+++ client/src/views/RootLayout/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import Footer from "src/components/Footer";
 import NavigationBar from "src/components/NavigationBar";
 
 const RootLayout = () => {
@@ -9,6 +10,7 @@ const RootLayout = () => {
         <NavigationBar />
       </Box>
       <Outlet />
+      <Footer />
     </>
   );
 };
