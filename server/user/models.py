3d7fd3cc51c52dee8c3e616f4b0417f83980e7eb
anup-an <friek_an@yahoo.com>
2023-12-13 17:22:42 +0200

    Update book and user model. Add migration files

--- server/user/models.py
+++ server/user/models.py
@@ -6,8 +6,6 @@ from book.models import BookInstance
 
 class User(AbstractUser):
     username = models.EmailField(max_length=255, unique=True)
-    books_onloan = models.ManyToManyField(
-        BookInstance, blank=True)
 
     def __str__(self):
         return self.username
